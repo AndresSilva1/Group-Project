bc8785a91b2486f765ab1f94af0a852e92e24886
Kama Corey McKenzie <kcm@Kamas-MBP.attlocal.net>
2023-04-25 16:36:42 -0700

    Updated parameters for API call in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,11 @@
 /** @format */
 
-//let ts = String(Date().timeIntervalSince1970);
 let search = document.getElementById("apiSearch");
 let searchBtn = document.getElementById("apiBtn");
-let ts = new Date().getTime();
 
-const [timestamp, apiKey, privKey, hashValue] = [
-  ts,
-  publicKey,
-  privateKey,
-  hashVal,
-];
-let stringToHash = timestamp + apiKey + privKey;
-let hash = md5(stringToHash);
+const [timestamp, apiKey, hashValue] = [ts, publicKey, hashVal];
 
-let url2 = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hash}&nameStartsWith=${search.value}`;
-
-const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hash}&nameStartsWith=${search.value}`;
+const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&nameStartsWith=${search.value}`;
 
 function fetchUrl() {
   fetch(url)
