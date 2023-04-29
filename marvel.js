27ce98af167272d1d321abc1051986b92f738450
Kama Corey McKenzie <kcm@Kamas-MBP.attlocal.net>
2023-04-29 09:06:48 -0700

    After api hero search is complete, js now returns hero name, description, and thumbnail. These three elements are now displayed on the page, but they need to be styled and formatted. Still ironing out a few issues with the search.

--- marvel.js
+++ marvel.js
@@ -1,7 +1,10 @@
 /** @format */
 
 let dataArray = [];
-let searchBtn = document.querySelector(".button");
+let chosenHero = "";
+let desc = "";
+let pic = "";
+let searchBtn = document.querySelector("#APIButton");
 let displaySuggestions = document.querySelector("#displaySuggestions");
 searchBtn.addEventListener("click", getData);
 let searchEnter = document.querySelector("#apiSearch");
@@ -16,9 +19,11 @@ const [timestamp, apiKey, hashValue] = [ts, publicKey, hashVal];
 
 async function getData() {
   let url = getUrlAPI();
+  console.log(url);
   // Clear existing suggestions
   clearSuggestions();
   fetchUrl(url);
+  setAPIData();
 }
 
 function getUrlAPI() {
@@ -26,7 +31,7 @@ function getUrlAPI() {
   let searchP = searchHero.value.trim();
   // Account for spaces
   searchP = searchP.replaceAll(" ", "%20");
-  let url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&nameStartsWith=${searchP}`;
+  let url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&name=${searchP}`;
   console.log(url);
   // Check for blank input
   if (searchP.length <= 0) {
@@ -74,12 +79,17 @@ apiSearch.addEventListener("keyup", async function () {
   fetchUrl(url);
 
   dataArray.forEach((suggestion) => {
-    let hero = suggestion.name;
-    console.log(hero);
+    let heroSug = suggestion.name;
+    chosenHero = heroSug;
+    desc = suggestion.description;
+    pic =
+      suggestion["thumbnail"]["path"] +
+      "." +
+      suggestion["thumbnail"]["extension"];
     let d = document.createElement("div");
     d.style.cursor = "pointer";
-    d.setAttribute("onclick", "displayHero('" + hero + "')");
-    d.innerHTML = "<p>" + hero + "</p>";
+    d.setAttribute("onclick", "displayHero('" + heroSug + "')");
+    d.innerHTML = "<p>" + heroSug + "</p>";
 
     document.querySelector("#suggestions").appendChild(d);
   });
@@ -89,6 +99,17 @@ apiSearch.addEventListener("keyup", async function () {
 
 function displayHero(hero) {
   searchEnter.value = hero;
+  chosenHero = hero;
   clearSuggestions();
   getData();
+  return;
 }
+
+function setAPIData() {
+  document.querySelector("#heroName").textContent = chosenHero;
+  document.querySelector("#heroDescription").innerHTML = "<p>" + desc + "</p>";
+  document.querySelector("#heroImage").setAttribute("src", pic);
+  return;
+}
+
+window.addEventListener("load", getData);
